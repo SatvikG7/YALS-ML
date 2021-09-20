e1e0c4ca775ff8d5c9137006aa1649791b41bef0
Satvik Gaikwad <satvik.g.code@gmail.com>
2021-09-20 23:05:52 +0530

    add made auth functional

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import '../styles/index.css'
+import { Provider } from "next-auth/client";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <Provider>
+            <Head>
+                <title>YALS</title>
+                <meta
+                    name="description"
+                    content="YALS-YetAnotherLinkShortener"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Component {...pageProps} />
+        </Provider>
+    );
 }
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,95 @@
 import type { NextPage } from "next";
-import Head from "next/head";
+import { signIn, signOut, useSession } from "next-auth/client";
+import Link from "next/link";
+import { useState } from "react";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-    return (
-        <div className="home-container">
-            <Head>
-                <title>YALS</title>
-                <meta
-                    name="description"
-                    content="YALS-YetAnotherLinkShortener"
-                />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+    const [active, setActive] = useState(false);
+    const [session, loading] = useSession();
+    console.log(session);
+    const handleClick = () => {
+        setActive(!active);
+    };
 
-            <main className="w-full">
-                <h1 className="text-center text-2xl md:text-4xl">
-                    Welcome to YetAnotherLinkShortener
-                </h1>
-            </main>
+    return (
+        <div className="flex flex-col justify-between h-fullpage">
+            <nav className="flex items-center flex-wrap bg-blue-400 p-3 ">
+                <Link href="/">
+                    <a className="inline-flex items-center p-2 mr-4 ">
+                        <span className="text-xl text-white font-bold tracking-wide">
+                            YetAnotherLinkShortener
+                        </span>
+                    </a>
+                </Link>
+                <button
+                    className=" inline-flex p-3 hover:bg-green-600 rounded lg:hidden text-white ml-auto hover:text-white outline-none"
+                    onClick={handleClick}
+                >
+                    <svg
+                        className="w-6 h-6"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M4 6h16M4 12h16M4 18h16"
+                        />
+                    </svg>
+                </button>
+
+                {/*Note that in this div we will use a ternary operator to decide whether or not to display the content of the div  */}
+                <div
+                    className={`${
+                        active ? "" : "hidden"
+                    } w-full lg:inline-flex lg:flex-grow lg:w-auto ${
+                        !session && loading ? "loading" : "loaded"
+                    }`}
+                >
+                    <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto">
+                        {!loading && !session ? (
+                            <Link href="/api/auth/signin">
+                                <a
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                        signIn();
+                                    }}
+                                    className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white"
+                                >
+                                    Sign in / Sign up
+                                </a>
+                            </Link>
+                        ) : (
+                            <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white">
+                                {session?.user?.name}
+                            </a>
+                        )}
+                        {session && (
+                            <Link href="/api/auth/signup">
+                                <a
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                        signOut();
+                                    }}
+                                    className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white"
+                                >
+                                    Sign out
+                                </a>
+                            </Link>
+                        )}
+                        <Link href="/">
+                            <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white">
+                                About
+                            </a>
+                        </Link>
+                    </div>
+                </div>
+            </nav>
             <section className="w-full">
                 <form
                     className="w-full flex md:flex-row flex-col items-center justify-evenly content-between"
