5ee96cfb90a958fb04932f2724b1da3b18f85933
Satvik Gaikwad <satvik.g.code@gmail.com>
2021-10-14 08:15:58 +0530

    Formatted

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,27 @@
 import type { NextPage } from "next";
-import {getSession} from 'next-auth/client'
+import { getSession } from "next-auth/client";
 const Home: NextPage = () => {
     return (
         <div className="flex flex-col justify-between">
-                <form
-                    className="w-full flex md:flex-row flex-col items-center justify-evenly content-between"
-                    action="/api/post/"
+            <form
+                className="w-full flex md:flex-row flex-col items-center justify-evenly content-between"
+                action="/api/post/"
                 method="POST"
-                >
-                    <input
-                        className="md:4/5 bg-green-500 placeholder-green-800 rounded-md h-14 px-3"
-                        type="url"
-                        name="url"
-                        id="url"
-                        placeholder="https://"
-                        required
-                    />
-                    <input
-                        className="md:w-1/5 bg-yellow-500 rounded-md h-14 min-w-min"
-                        type="submit"
-                        value="Generate Shorted Link"
-                    />
-                </form>
+            >
+                <input
+                    className="md:4/5 bg-green-500 placeholder-green-800 rounded-md h-14 px-3"
+                    type="url"
+                    name="url"
+                    id="url"
+                    placeholder="https://"
+                    required
+                />
+                <input
+                    className="md:w-1/5 bg-yellow-500 rounded-md h-14 min-w-min"
+                    type="submit"
+                    value="Generate Shorted Link"
+                />
+            </form>
         </div>
     );
 };
