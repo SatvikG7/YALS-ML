ef16ada6b238acadfe08f426f1c332243ada70c7
Satvik Gaikwad <satvik.g.code@gmail.com>
2021-11-27 19:10:16 +0530

    Removed Microsoft Clarity script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -30,19 +30,6 @@ function MyApp({ Component, pageProps }: AppProps) {
 
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <Script
-                id="clarity"
-                dangerouslySetInnerHTML={{
-                    __html: `
-                    (function(c,l,a,r,i,t,y){
-                        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
-                        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                    })(window, document, "clarity", "script", "9htedwvupc");
-          `,
-                }}
-            />
-            
             <div id="app">
                 <Navbar />
                 <Component {...pageProps} />
