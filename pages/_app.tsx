f9abe99a0f0834c27176c17e611fcbab79303887
Satvik Gaikwad <satvik.g.code@gmail.com>
2021-11-27 11:08:07 +0530

    Added meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,11 +9,23 @@ function MyApp({ Component, pageProps }: AppProps) {
     return (
         <Provider session={pageProps.session}>
             <Head>
-                <title>YALS</title>
+                <title>YALS - Yet Another Link Shortener</title>
                 <meta
                     name="description"
                     content="YALS-YetAnotherLinkShortener"
                 />
+                <meta name="robots" content="index, follow" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                />
+
+                <meta
+                    name="keywords"
+                    content="YALS, link shortener, short link, small link, minify url, url shortener"
+                />
+                <meta name="author" content="SatvikG7" />
+
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
