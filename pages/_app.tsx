dbbea7cc32ae972eb65dd2469def3eee56928d40
Satvik Gaikwad <satvik.g.code@gmail.com>
2021-11-27 11:26:41 +0530

    Added Microsoft Clarity script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,8 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Script from "next/script";
+
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <Provider session={pageProps.session}>
@@ -28,7 +30,19 @@ function MyApp({ Component, pageProps }: AppProps) {
 
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-
+            <Script
+                id="clarity"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                    (function(c,l,a,r,i,t,y){
+                        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+                        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                    })(window, document, "clarity", "script", "9htedwvupc");
+          `,
+                }}
+            />
+            
             <div id="app">
                 <Navbar />
                 <Component {...pageProps} />
