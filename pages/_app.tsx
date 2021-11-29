5265bc9cc54e3c69ac9c69c120a14c64934a147d
Satvik Gaikwad <satvik.g.code@gmail.com>
2021-11-29 15:11:28 +0530

    Remove unused imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,6 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
