d6a7738e856c2b168722be5fd44a7c8e0fed3dd8
Satvik Gaikwad <satvik.g.code@gmail.com>
2021-12-17 20:36:31 +0530

    Add: sitemap.xml api route

--- pages/api/sitemap.xml.ts
+++ pages/api/sitemap.xml.ts
@@ -1,23 +1,42 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+import axios from "axios";
+import type { AxiosRequestConfig } from "axios";
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	let api_response;
+	let headersList = {
+		Accept: "*/*",
+		Authorization: `Bearer ${process.env.VERCEL_API_TOKEN}`,
+		"Content-Type": "application/json",
+	};
+
+	let reqOptions: AxiosRequestConfig = {
+		url: process.env.VERCEL_API_URL,
+		method: "GET",
+		headers: headersList,
+	};
+	let lastmod: Date;
+	const response = await axios(reqOptions);
+	lastmod = await new Date(response.data.deployments[0].ready);
 	const data = `
 	<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 		<url>
 			<loc>https://yals.ml/</loc>
-			<lastmod>2021-12-15T08:04:23+00:00</lastmod>
+			<lastmod>${await lastmod.toISOString().split("T")[0]}</lastmod>
 			<changefreq>weekly</changefreq>
 			<priority>1.0</priority>
 		</url>
 		<url>
 			<loc>https://yals.ml/api/auth/signin/</loc>
-			<lastmod>2021-12-15T08:04:23+00:00</lastmod>
-			<changefreq>monthly</changefreq>
+			<lastmod>${await lastmod.toISOString().split("T")[0]}</lastmod>
+			<changefreq>weekly</changefreq>
 			<priority>0.9</priority>
 		</url>
 	</urlset>
 	`;
-	res.status(200)
-		.setHeader("Content-Type", "application/xml")
-		.send(data);
+	await res.status(200).setHeader("Content-Type", "application/xml").send(data);
 }
